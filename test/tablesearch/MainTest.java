51d5326e7892dc28e2749f00dd81fd4c039beaa5
dworden <dworden@VDI-DWorden.cfglife.com>
2019-07-31 13:11:23 -0400

    Adding DriverFactory

--- test/tablesearch/MainTest.java
+++ test/tablesearch/MainTest.java
@@ -1,13 +1,20 @@
 package tablesearch;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainTest {
+	enum Browser {FireFox,Chrome,IE}
+	
+	public static final String TableTestPage = "file:" + System.getProperty("user.dir") + "/src/AutomationTest.html";
+
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.get("AutomationTest.html");
+		//Select which browser to user here
+		Browser browser = Browser.Chrome;		
+		WebDriver driver = DriverFactory.getNewDriver(browser);
+		
+		//Create a Method to get the page using the url in TableTestPage and Click the link in the row that contains the payor role. 
+		
 	}
 
 }
