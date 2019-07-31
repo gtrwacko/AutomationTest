c594e99c571834340fc637e048fcbb479f6f2ebc
dworden <dworden@VDI-DWorden.cfglife.com>
2019-07-31 12:45:34 -0400

    Initial Commit

---
+++ test/tablesearch/MainTest.java
@@ -0,0 +1,13 @@
+package tablesearch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("AutomationTest.html");
+	}
+
+}
